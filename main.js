6fcda6af50bc2c82a31d3d04ed615b0219dac635
Oliver Njiru <olivernjiru@gmail.com>
2022-02-17 21:58:29 +0300

    declaring varibles with let and const

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-// running javascript //
+// To output to the console from the script 
 // console.log("Hello from JavaScript");
 
-alert('Hello World')
+// To output an alert message 
+// alert("Hello World")
+
+//To output an error in the console from the script
+// console.error("This is an error")
+
+// To output a warning in the console from the script
+// console.warn("This is a warning")
+
+// How to set variables. We use var, let and const
+// With let, you can reassign values, e.g:
+// let age = 30;
+// age = 31;
+// console.log(age)
+
+//With const, you can olny assign a variable once, otherwise, it will bring an error, e.g:
+// const age = 30;
+// age = 31;
+// console.log(age)
+// Always use const unless you know you're gonna reassign a value like in the case of a game when there is a score update, the use let to update the score after some code runs
+// Also if and when you use const, you have to add a value when declaring a variable
+//When you are using arrays, you can change the values in the array but not reassign the entire value again
+
+// JS Data Types: String, Numbers, Boolean, null, undefined and symbol
+
+
