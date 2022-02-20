733bf2e10329a5729eb586d6bd931558035868fb
Oliver Njiru <olivernjiru@gmail.com>
2022-02-20 19:37:20 +0300

    did some js commenting and polishing

--- main.js
+++ main.js
@@ -15,7 +15,6 @@
 // let age = 30;
 // age = 31;
 // console.log(age)
-
 //With const, you can olny assign a variable once, otherwise, it will bring an error, e.g:
 // const age = 30;
 // age = 31;
@@ -27,27 +26,20 @@
 // JS Data Types: String, Numbers, Boolean, null, undefined and symbol
 //String can have double or single quotes and they also don't need semi-colons
 // const name = 'John'
-
 // Number can support both whole numbers and decimals too
 // const age = 30;
 // const rating = 4.5;
-
 //Boolean has no quotes, otherwise it is going to be a string
 // const isCool = true
-
 //Null means declaring a variable to be empty
 // const x =null;
-
 // Undefined can be explicitly defined as the first code below or initialized as the latter code
 // const y = undefined
 // let z;
-
 // To test the type of a variable
 // console.log(typeof name)
-
 // Concatenation
 // console.log("My name is " + name + " and my age is " + age);
-
 // Template strings, we use backstrokes
 // console.log(`My name is ${name} and my age os ${age}`)
 // And we can also assign the above to a variable such as below:
